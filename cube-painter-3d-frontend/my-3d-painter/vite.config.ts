bc3596eb2b9abbd1ee27f042e42baccfaa96daaf
kj_zxh <zkelvins@qq.com>
2025-11-05 16:29:25 +0800

    init

--- cube-painter-3d-frontend/my-3d-painter/vite.config.ts
+++ cube-painter-3d-frontend/my-3d-painter/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// 1. 从插件中导入需要用到的函数
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 2. 配置 Element Plus 的按需自动导入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
 })
